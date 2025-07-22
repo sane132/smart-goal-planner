d170524afc5fb86a6c9677ea929d3b4c2daaf067
Mohamed <muhamadsane114@gmail.com>
2025-07-22 08:21:03 +0300

    add

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // src/api.js
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'http://localhost:3001';
 
 export const getGoals = async () => {
     const response = await fetch(`${API_BASE_URL}/goals`);

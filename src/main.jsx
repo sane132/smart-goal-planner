c0db8fb6fdc44f7eca82ae3468855ba043e726b9
Mohamed <muhamadsane114@gmail.com>
2025-07-23 00:22:08 +0300

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-
-function App() {
-  return (
-    <div>
-      <h1>Welcome to Smart Goal Planner!</h1>
-    </div>
-  )
-}
+import App from './App'
+import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

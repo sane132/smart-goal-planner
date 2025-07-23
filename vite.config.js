9b5ae0f5d3afa83ae776b3c6be007b97a543ce41
Mohamed <muhamadsane114@gmail.com>
2025-07-23 15:37:18 +0300

    update some files

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  base: '/smart-goal-planner/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  }
+})

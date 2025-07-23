0d43b5521f4185d4dbbd682ba4a9aa8ad28092c4
Mohamed <muhamadsane114@gmail.com>
2025-07-23 13:03:40 +0300

    update App.jsx

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/goals': 'http://localhost:3000',
+    },
+  },
+});
